98f9838721b22636fa0a70e786e056adb61b6dd1
miguelavg <miguelavg@chevere>
2012-10-12 00:07:16 -0500

    Listo

---
+++ src/flytrack/FlyTrack.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flytrack;
+
+/**
+ *
+ * @author miguelavg
+ */
+public class FlyTrack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
