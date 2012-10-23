f0a110c49192e81b5b57a5d5433ccc3068db0360
msolorzano <manuel.solorzanoc@gmail.com>
2012-10-23 17:17:59 -0500

    modificaciones del nombre perfil

--- src/gui/administracion/Perfil_Modificar.java
+++ src/gui/administracion/Perfil_Modificar.java
@@ -8,12 +8,12 @@ package gui.administracion;
  *
  * @author msolorzano
  */
-public class Rol_Modificar extends javax.swing.JFrame {
+public class Perfil_Modificar extends javax.swing.JFrame {
 
     /**
-     * Creates new form Rol_Modificar
+     * Creates new form Perfil_Modificar
      */
-    public Rol_Modificar() {
+    public Perfil_Modificar() {
         initComponents();
     }
 
@@ -299,20 +299,20 @@ public class Rol_Modificar extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Rol_Modificar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Perfil_Modificar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Rol_Modificar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Perfil_Modificar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Rol_Modificar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Perfil_Modificar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Rol_Modificar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Perfil_Modificar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Rol_Modificar().setVisible(true);
+                new Perfil_Modificar().setVisible(true);
             }
         });
     }
