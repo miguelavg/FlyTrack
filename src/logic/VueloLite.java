9db894dd5a2bb19d4db555ad9614f890336248b2
miguelavg <miguelavg@chevere>
2012-11-04 14:47:45 -0500

    clases para el algoritmo completas

--- src/logic/VueloLite.java
+++ src/logic/VueloLite.java
@@ -4,10 +4,48 @@
  */
 package logic;
 
+import beans.Aeropuerto;
+
 /**
  *
  * @author miguelavg
  */
 public class VueloLite {
-    
+
+    private Aeropuerto origen;
+    private Aeropuerto destino;
+    private double pLleno;
+
+    public VueloLite(Aeropuerto origen, Aeropuerto destino, double pLleno) {
+        this.origen = origen;
+        this.destino = destino;
+        this.pLleno = pLleno;
+    }
+
+    public VueloLite() {
+    }
+
+    public Aeropuerto getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(Aeropuerto origen) {
+        this.origen = origen;
+    }
+
+    public Aeropuerto getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Aeropuerto destino) {
+        this.destino = destino;
+    }
+
+    public double getpLleno() {
+        return pLleno;
+    }
+
+    public void setpLleno(double pLleno) {
+        this.pLleno = pLleno;
+    }
 }
