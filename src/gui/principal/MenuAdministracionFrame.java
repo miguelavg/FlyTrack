be751881c1ea8fd60e6940e8b7628bf9f10ab8b1
msolorzano <manuel.solorzanoc@gmail.com>
2012-11-06 13:00:28 -0500

    modificaciones en las ventanas para que funcione los perfiles

--- src/gui/principal/MenuAdministracionFrame.java
+++ src/gui/principal/MenuAdministracionFrame.java
@@ -4,12 +4,16 @@
  */
 package gui.principal;
 
+import beans.Sesion;
+import beans.seguridad.Permiso;
+import controllers.CSeguridad;
 import gui.administracion.tarifas.TarifaFrame;
 import gui.administracion.vuelos.Vuelos;
 import gui.administracion.aeropuertos.Aeropuerto;
 import gui.administracion.tipocambio.TipoCambioDialog;
 import gui.clientes.ClientesPopUp;
 import gui.seguridad.parametros.ParametroDialog;
+import java.util.List;
 
 /**
  *
@@ -22,6 +26,22 @@ public class MenuAdministracionFrame extends javax.swing.JDialog {
      */
     public MenuAdministracionFrame() {
         initComponents();
+        
+        List<Permiso> permisos = Sesion.getUsuario().getPerfil().getPermisos();
+        boolean tarifas = CSeguridad.validarPermiso(2, "Administracion", "Tarifas", permisos);
+        this.btnTarifas.setEnabled(tarifas);
+        this.lblTarifas.setEnabled(tarifas);
+        boolean tipoCambio = CSeguridad.validarPermiso(2, "Administracion", "TipoCambio", permisos);
+        this.btnTipoCambio.setEnabled(tipoCambio);
+        this.lblTipoCambio.setEnabled(tipoCambio);
+        boolean vuelos = CSeguridad.validarPermiso(2, "Administracion", "Vuelos", permisos);
+        this.btnVuelos.setEnabled(vuelos);
+        this.lblVuelos.setEnabled(vuelos);
+        boolean aeropuertos = CSeguridad.validarPermiso(2, "Administracion", "Aeropuertos", permisos);
+        this.btnAeropuertos.setEnabled(aeropuertos);
+        this.lblAeropuertos.setEnabled(aeropuertos);
+        
+        pack();
     }
 
     /**
@@ -33,51 +53,52 @@ public class MenuAdministracionFrame extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        tarifaButton = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        vueloButton = new javax.swing.JButton();
-        aeropuertoButton = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        tarifaButton1 = new javax.swing.JButton();
+        btnTarifas = new javax.swing.JButton();
+        lblTarifas = new javax.swing.JLabel();
+        btnVuelos = new javax.swing.JButton();
+        btnAeropuertos = new javax.swing.JButton();
+        lblVuelos = new javax.swing.JLabel();
+        lblAeropuertos = new javax.swing.JLabel();
+        lblTipoCambio = new javax.swing.JLabel();
+        btnTipoCambio = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setModal(true);
+        setResizable(false);
 
-        tarifaButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tarifa48x48.png"))); // NOI18N
-        tarifaButton.addActionListener(new java.awt.event.ActionListener() {
+        btnTarifas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tarifa48x48.png"))); // NOI18N
+        btnTarifas.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                tarifaButtonActionPerformed(evt);
+                btnTarifasActionPerformed(evt);
             }
         });
 
-        jLabel1.setText("Tarifas");
+        lblTarifas.setText("Tarifas");
 
-        vueloButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/vuelo48x48.png"))); // NOI18N
-        vueloButton.addActionListener(new java.awt.event.ActionListener() {
+        btnVuelos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/vuelo48x48.png"))); // NOI18N
+        btnVuelos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                vueloButtonActionPerformed(evt);
+                btnVuelosActionPerformed(evt);
             }
         });
 
-        aeropuertoButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/aeropuerto48x48.png"))); // NOI18N
-        aeropuertoButton.addActionListener(new java.awt.event.ActionListener() {
+        btnAeropuertos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/aeropuerto48x48.png"))); // NOI18N
+        btnAeropuertos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                aeropuertoButtonActionPerformed(evt);
+                btnAeropuertosActionPerformed(evt);
             }
         });
 
-        jLabel2.setText("  Vuelos ");
+        lblVuelos.setText("  Vuelos ");
 
-        jLabel3.setText("Aeropuerto");
+        lblAeropuertos.setText("Aeropuertos");
 
-        jLabel4.setText("Tipo de cambio");
+        lblTipoCambio.setText("Tipo de cambio");
 
-        tarifaButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tipocambio48x48.png"))); // NOI18N
-        tarifaButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnTipoCambio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tipocambio48x48.png"))); // NOI18N
+        btnTipoCambio.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                tarifaButton1ActionPerformed(evt);
+                btnTipoCambioActionPerformed(evt);
             }
         });
 
@@ -88,30 +109,30 @@ public class MenuAdministracionFrame extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addGap(45, 45, 45)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(tarifaButton)
+                    .addComponent(btnTarifas)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(12, 12, 12)
-                        .addComponent(jLabel1)))
+                        .addComponent(lblTarifas)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(45, 45, 45)
-                        .addComponent(tarifaButton1))
+                        .addComponent(btnTipoCambio))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(18, 18, 18)
-                        .addComponent(jLabel4)))
+                        .addComponent(lblTipoCambio)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(vueloButton)
-                    .addComponent(jLabel2))
+                    .addComponent(btnVuelos)
+                    .addComponent(lblVuelos))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(45, 45, 45)
-                        .addComponent(aeropuertoButton)
+                        .addComponent(btnAeropuertos)
                         .addGap(45, 45, 45))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel3)
-                        .addGap(35, 35, 35))))
+                        .addComponent(lblAeropuertos)
+                        .addGap(29, 29, 29))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -120,21 +141,21 @@ public class MenuAdministracionFrame extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(vueloButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(aeropuertoButton))
+                            .addComponent(btnVuelos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnAeropuertos))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(lblAeropuertos, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblVuelos, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(tarifaButton1)
+                            .addComponent(btnTipoCambio)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(tarifaButton)
+                                .addComponent(btnTarifas)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                    .addComponent(lblTarifas, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(lblTipoCambio, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE))))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -142,32 +163,32 @@ public class MenuAdministracionFrame extends javax.swing.JDialog {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void tarifaButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tarifaButtonActionPerformed
+    private void btnTarifasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTarifasActionPerformed
         // TODO add your handling code here:
         TarifaFrame tarifa = new TarifaFrame();
         tarifa.setVisible(true);
-    }//GEN-LAST:event_tarifaButtonActionPerformed
+    }//GEN-LAST:event_btnTarifasActionPerformed
 
-    private void vueloButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_vueloButtonActionPerformed
+    private void btnVuelosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVuelosActionPerformed
         // TODO add your handling code here:
         Vuelos vuelo = new Vuelos();
         //vuelo.setModal(true);
         vuelo.setVisible(true);
-    }//GEN-LAST:event_vueloButtonActionPerformed
+    }//GEN-LAST:event_btnVuelosActionPerformed
 
-    private void aeropuertoButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aeropuertoButtonActionPerformed
+    private void btnAeropuertosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAeropuertosActionPerformed
         // TODO add your handling code here:
         Aeropuerto aeropuerto = new Aeropuerto();
         aeropuerto.setModal(true);
         aeropuerto.setVisible(true);
        
-    }//GEN-LAST:event_aeropuertoButtonActionPerformed
+    }//GEN-LAST:event_btnAeropuertosActionPerformed
 
-    private void tarifaButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tarifaButton1ActionPerformed
+    private void btnTipoCambioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTipoCambioActionPerformed
         // TODO add your handling code here:
         TipoCambioDialog tipoCambioDialog = new TipoCambioDialog();
         tipoCambioDialog.setVisible(true);
-    }//GEN-LAST:event_tarifaButton1ActionPerformed
+    }//GEN-LAST:event_btnTipoCambioActionPerformed
 
     /**
      * @param args the command line arguments
@@ -211,13 +232,13 @@ public class MenuAdministracionFrame extends javax.swing.JDialog {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton aeropuertoButton;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JButton tarifaButton;
-    private javax.swing.JButton tarifaButton1;
-    private javax.swing.JButton vueloButton;
+    private javax.swing.JButton btnAeropuertos;
+    private javax.swing.JButton btnTarifas;
+    private javax.swing.JButton btnTipoCambio;
+    private javax.swing.JButton btnVuelos;
+    private javax.swing.JLabel lblAeropuertos;
+    private javax.swing.JLabel lblTarifas;
+    private javax.swing.JLabel lblTipoCambio;
+    private javax.swing.JLabel lblVuelos;
     // End of variables declaration//GEN-END:variables
 }
