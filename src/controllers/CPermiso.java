3ac25c88bf97fb43320745f5e04b560c1df00c71
msolorzano <manuel.solorzanoc@gmail.com>
2012-11-09 11:24:24 -0500

    avazando perfiles 2

---
+++ src/controllers/CPermiso.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import beans.seguridad.Permiso;
+import java.util.List;
+
+/**
+ *
+ * @author msolorzano
+ */
+public class CPermiso {
+
+    public static boolean buscarPermiso(List<Permiso> permisos, String nombre, int nivel, String nombrePadre){
+        
+        return false;
+    }
+}
