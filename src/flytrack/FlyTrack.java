9ecc5479e0216b25e90324de9d70db7004306877
ronald <ronald@hpvb>
2012-11-26 19:48:57 -0500

    añadiendo wizard

--- src/flytrack/FlyTrack.java
+++ src/flytrack/FlyTrack.java
@@ -5,6 +5,10 @@
 package flytrack;
 
 import gui.principal.Login;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  *
@@ -15,8 +19,29 @@ public class FlyTrack {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Login login = new Login();
-        login.setVisible(true);
+    public static String leerSentencias(String archivo){
+        String content = "";
+        try{
+            content = new Scanner(new File(archivo)).useDelimiter("\\Z").next();
+        }
+        catch(Exception e){
+            
+        }
+        return content;
+        
     }
+    
+    public static void main(String[] args) {
+        String content = leerSentencias("/home/gg/gg.txt");
+        if (content.contains("no")){
+            Login login = new Login();
+            login.setVisible(true);
+        }
+        else if (content.contains("si")) {
+            wizardbd.WizardFrame0 wf0 = new wizardbd.WizardFrame0();
+            wf0.setVisible(true);
+        }
+    }
+
+  
 }
